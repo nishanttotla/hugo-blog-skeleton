605b9fcc96f9e1ea398ff53c19e1c59bf3d69fab
Nishant Totla <nishanttotla@gmail.com>
2016-03-07 15:29:44 -0800

    Updating config and release script
    Signed-off-by: Nishant Totla <nishanttotla@gmail.com>

--- release.sh
+++ release.sh
@@ -3,7 +3,7 @@ echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 hugo --theme=skeleton
 cd public
 git add -A
-msg="rebuilding site `date`"
+msg="Rebuilding Site `date`"
 if [ $# -eq 1 ]
   then msg="$1"
 fi
