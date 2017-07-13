20b76b33c02ae43ae505fdd5044ec4047e7ead60
Nishant Totla <nishanttotla@gmail.com>
2017-07-13 11:03:32 -0700

    imagemin gulpfile minifies all images
    Signed-off-by: Nishant Totla <nishanttotla@gmail.com>

--- static/gulpfile.js
+++ static/gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
     imagemin = require('gulp-imagemin');
 
 gulp.task('default', function() {
-  gulp.src('images/*.jpg')
+  gulp.src(['images/**/*'])
       .pipe(imagemin())
       .pipe(gulp.dest('images'))
 });
